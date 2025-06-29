58c13d99294d59201e06b28c24f4480c31d6ff8f
AlejoMaidana <alejomaidana29@gmail.com>
2025-06-29 01:11:49 -0300

    planteo de paleta redireccion de archivos y nuevos fondos

--- Inevstigacion 2025/aplicacion web INTA/front-end/static/js/login.js
+++ Inevstigacion 2025/aplicacion web INTA/front-end/static/js/login.js
@@ -7,7 +7,9 @@ function validarInicioSesion() {
     } else {
         // alert('Campos llenos, redirigiendo...');
         // Redirigir a la página principal
-        window.location.href = '../menu-principal/pagina_principal.html';  // Aquí colocas la ruta correcta a tu página principal
+        window.location.href = './pagina_principal.html';
+
+  // Aquí colocas la ruta correcta a tu página principal
     }
 }
 
